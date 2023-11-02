849115ce3afdd30e7d5dfe5e0cb0da1960a55136
Luigi Pio Nargiso <nargiso.luigipio@gmail.com>
2023-11-02 18:55:10 +0100

    tracking panel added

---
+++ src/tracking_panel.py
@@ -0,0 +1,22 @@
+import bpy
+from .default_panel import DefaultPanel
+
+
+class TrackingPanel(DefaultPanel, bpy.types.Panel):
+    bl_label = "Tracking"
+    bl_parent_id = "hta_default_panel"
+    bl_idname = "hta_trackingpanel"
+
+    def draw(self, context):
+        layout = self.layout
+
+        layout.prop(context.scene.handtracktool, "camera_device_slot")
+        layout.prop(context.scene.handtracktool, "skip_frames")
+
+
+def register():
+    bpy.utils.register_class(TrackingPanel)
+
+
+def unregister():
+    bpy.utils.unregister_class(TrackingPanel)
